c08c62b51316b682a95fb5ac17f61e6b4d84bb54
KercyDing <dkx215417@gmail.com>
2026-07-28 21:37:27 +0800

    feat: 切换 Glow 渲染后端

--- src/app.rs
+++ src/app.rs
@@ -835,7 +835,7 @@ impl eframe::App for App {
         ui::render(self, root);
     }
 
-    fn on_exit(&mut self) {
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
         self.shutdown_on_exit();
     }
 }
